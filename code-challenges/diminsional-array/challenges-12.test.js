d4cab4c043b64da181b49e8f03cdce3524d81a6d
alaalmasri12 <alaa_almasri1997@yahoo.com>
2020-04-14 01:41:03 +0300

    sada

--- code-challenges/diminsional-array/challenges-12.test.js
+++ code-challenges/diminsional-array/challenges-12.test.js
@@ -175,7 +175,7 @@ TESTS
 All the code below will verify that your functions are working to solve the challenges.
 DO NOT CHANGE any of the below code.
 Run your tests from the console: jest challenge-12.test.js
----------------------------------------------------------------------------------------------- */
+------------------------------------------------------------------------------------------ */
 
 
 describe('Testing challenge 1', () => {
